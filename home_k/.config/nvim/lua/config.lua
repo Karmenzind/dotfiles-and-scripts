11cfc07b0c405657d2eb3ab83298c6165df65335
K <valesail7@gmail.com>
2023-09-14 21:07:06 +0800

    :eyes: fixed nvim mappings

--- home_k/.config/nvim/lua/config.lua
+++ home_k/.config/nvim/lua/config.lua
@@ -3,24 +3,24 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
+local function nvim_tree_on_attach(bufnr)
+    local api = require('nvim-tree.api')
+    local function opts(desc)
+      return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.del('n', 's', { buffer = bufnr })
+    vim.keymap.set('n', 's', api.node.open.vertical, opts('Split'))
+    vim.keymap.set('n', 'i', api.node.open.horizontal, opts('VSplit'))
+    vim.keymap.set('n', 't', api.node.open.tab, opts('NewTab'))
+    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+    vim.keymap.set('n', '<leader>n', api.tree.close, opts('Close'))
+end
 
 require("nvim-tree").setup({
-    on_attach = function(bufnr)
-        local api = require('nvim-tree.api')
-        local function opts(desc)
-          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        end
-
-        api.config.mappings.default_on_attach(bufnr)
-
-        vim.keymap.del('n', 's', { buffer = bufnr })
-        vim.keymap.set('n', 's', api.node.open.vertical)
-        vim.keymap.set('n', 'i', api.node.open.horizontal)
-        vim.keymap.set('n', 't', api.node.open.tab)
-        vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-        vim.keymap.set('n', '<leader>n', api.tree.close, opts('Close'))
-    end,
-
+    on_attach = nvim_tree_on_attach,
     view = {
         float = { enable = false, open_win_config = { border = "double" } },
     },
